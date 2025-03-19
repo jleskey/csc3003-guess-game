a04a03bba722d99edc832ea3c5de7e64fd2445ae
Joseph Leskey <joseph@jleskey.com>
2025-03-19 15:59:33 -0400

    Initialize repository

---
+++ src/GuessGame.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const port = 8080;
+
+// Using body-parser allows you to access request.body 
+// from within routes and use that data.
+app.use(bodyParser.urlencoded({extended:true}));
+
+const form = '<h2>Welcome to Guess the Number Game!</h2>' +
+             '<p><strong>To play, guess a number between 1 and 100.</strong></p>' +
+             '<form action="/" method="post">' + 
+			 '<p><label for="guess">Enter your guess: </label>' +
+			 '<input type="number" name="guess"></p>' +
+			 '<input type="submit">' +
+			 '</form>'
+
+app.get('/', function(request, response, next){	
+	response.send(form);
+});
+
+app.post('/', function(request, response, next){
+	response.send(form + '<p>Your guess is: ' + request.body.guess + '</p>');
+});
+
+// Returns a random number between min and max, inclusive
+function between(min, max){  
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+app.listen(port);
