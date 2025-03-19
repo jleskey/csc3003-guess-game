2f686b68c64d9b2292ba42ee4fcfaa5cefab9629
Joseph Leskey <joseph@jleskey.com>
2025-03-19 16:07:19 -0400

    Modernize codebase

--- src/GuessGame.mjs
+++ src/GuessGame.mjs
@@ -1,32 +1,32 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 const port = 8080;
 
-// Using body-parser allows you to access request.body 
+// Using body-parser allows you to access request.body
 // from within routes and use that data.
 app.use(bodyParser.urlencoded({extended:true}));
 
 const form = '<h2>Welcome to Guess the Number Game!</h2>' +
              '<p><strong>To play, guess a number between 1 and 100.</strong></p>' +
-             '<form action="/" method="post">' + 
+             '<form action="/" method="post">' +
 			 '<p><label for="guess">Enter your guess: </label>' +
 			 '<input type="number" name="guess"></p>' +
 			 '<input type="submit">' +
 			 '</form>'
 
-app.get('/', function(request, response, next){	
+app.get('/', (request, response, next) => {
 	response.send(form);
 });
 
-app.post('/', function(request, response, next){
-	response.send(form + '<p>Your guess is: ' + request.body.guess + '</p>');
+app.post('/', (request, response, next) => {
+	response.send(`${form}<p>Your guess is: ${request.body.guess}</p>`);
 });
 
 // Returns a random number between min and max, inclusive
-function between(min, max){  
+function between(min, max){
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
