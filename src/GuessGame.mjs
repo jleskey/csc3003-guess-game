7107abeac03963c3b3852891fc95acbdcac5a5b8
Joseph Leskey <joseph@jleskey.com>
2025-03-21 16:11:49 -0400

    Restructure for game loop

--- src/GuessGame.mjs
+++ src/GuessGame.mjs
@@ -9,14 +9,36 @@ import bodyParser from 'body-parser';
 
 const port = 8080;
 
+// Set up an Express app.
 const app = setup(port);
 
-let number;
+// Attach the game loop.
+hostGameLoop('/', app);
+
+/**
+ * Attach and initialize the game loop.
+ *
+ * @param {string} path The host path
+ * @param {express.Express} app An app to act as host
+ */
+function hostGameLoop(path, app) {
+	let number;
+
+	app.post(path, (req, res) => {
+		const guess = req.body.guess;
+		const reply = (body) => res.send(html(body));
+
+		if (!guess) {
+			reply('You didn\'t even try to guess? :(');
+		}
+	});
+}
 
 /**
  * Hosts the game on a local server.
  *
  * @param {number} port Port number
+ * @returns {express.Express} An express app
  */
 function setup(port) {
 	const app = express();
@@ -25,26 +47,47 @@ function setup(port) {
 	// routes and use that data.
 	app.use(bodyParser.urlencoded({ extended: true }));
 
-	const form =
-	'<h2>Welcome to Guess the Number Game!</h2>' +
-	'<p><strong>To play, guess a number between 1 and 100.</strong></p>' +
-	'<form action="/" method="post">' +
-	'<p><label for="guess">Enter your guess: </label>' +
-	'<input type="number" name="guess"></p>' +
-	'<input type="submit">' +
-	'</form>';
-
 	app.get('/', (_, res) => {
-		res.send(form);
-	});
-
-	app.post('/', (req, res) => {
-		res.send(`${form}<p>Your guess is: ${req.body.guess}</p>`);
+		res.send(html());
 	});
 
 	app.listen(port, () => console.log(`Listening on port ${port}.`));
 
 	return app;
+}
+
+/**
+ * Crafts an HTML response with the given content.
+ *
+ * @param {string?} content Content to display in the body of the HTML
+ * @returns {string} The given content, wrapped in appropriate HTML
+ */
+function html(content, isParagraph = true) {
+	const insert = content
+		? `\n${isParagraph ? `<p>${content}</p>` : content}`
+		: '';
+	return `
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<title>Joseph's Guessing Game</title>
+				<meta charset="utf-8">
+			</head>
+			<body>
+				<h2>Welcome to Guess the Number Game!</h2>
+				<p>
+					<strong>To play, guess a number between 1 and 100.</strong>
+				</p>
+				<form action="/" method="post">
+					<p>
+						<label for="guess">Enter your guess: </label>
+						<input type="number" name="guess">
+					</p>
+					<input type="submit">
+				</form>${insert}
+			</body>
+		</html>
+	`;
 }
 
 /**
